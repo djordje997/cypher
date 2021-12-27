061564d4f82ac954674be09f82381f7479ec15c6
djordje997 <86196338+djordje997@users.noreply.github.com>
2021-12-27 16:01:55 +0100

    line 15 fixes print of 2 array values with each_value[-1] always puts last value of array in result.

--- cypher.rb
+++ cypher.rb
@@ -1,15 +1,20 @@
-def rot(number_of_rotation)
-    puts 'Please enter your text'
-    user_input = gets.chomp
-    aplhabet = ("a".."z").to_a
-    user_input_to_array= user_input.split("")
-    result=""
-    for each_value in user_input_to_array
-        number_of_rotation.times do
-            each_value=each_value.next 
-        end
-        result << each_value
+class Rot
+    def rot(number_of_rotation)
+        puts 'Please enter your text'
+        user_input = gets.chomp
+        aplhabet = ("a".."z").to_a
+        aplhabet2 = ("A".."Z").to_a
+        user_input_to_array= user_input.split("")
+        result=""
+        for each_value in user_input_to_array
+            if aplhabet.include?(each_value) || aplhabet2.include?(each_value)
+            number_of_rotation.times do
+                each_value=each_value.next
+                end
+            end
+                result << each_value[-1] 
+            end
+    p result
     end
-p result
 end
-rot(5)
+var=Rot.new.rot(13)
