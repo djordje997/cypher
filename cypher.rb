ef4714bfb3bfef5d21ffd2c9c0adb9e1a869e6bc
djordje997 <86196338+djordje997@users.noreply.github.com>
2021-12-27 13:39:15 +0100

    iterating over each element of array of user input and using method parameter to call method next on value number times of given parameter,method next will change value to next characher in alphabet

--- cypher.rb
+++ cypher.rb
@@ -3,8 +3,13 @@ def rot(number_of_rotation)
     user_input = gets.chomp
     aplhabet = ("a".."z").to_a
     user_input_to_array= user_input.split("")
-    p user_input_to_array
-    p aplhabet
+    for each_value in user_input_to_array
+        number_of_rotation.times do
+            each_value=each_value.next
+            
+        end
+        p each_value.to_s
+    end
 
 end
 rot(1)
