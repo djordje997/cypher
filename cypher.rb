d50d62659a7ad7ef2fd9c24380c07e7c79fa5ab4
djordje997 <86196338+djordje997@users.noreply.github.com>
2021-12-28 12:41:57 +0100

    Constant ALPHABET added outside of class it can be used globaly, also constant use both lowercase and uppercase

--- cypher.rb
+++ cypher.rb
@@ -1,13 +1,14 @@
+ALPHABET= ("a".."z").to_a + ("A".."Z").to_a
+
 class Rot
+    
     def rot(number_of_rotation)
         puts 'Please enter your text'
         user_input = gets.chomp
-        aplhabet = ("a".."z").to_a
-        aplhabet2 = ("A".."Z").to_a
         user_input_to_array= user_input.split("")
         result=""
         for each_value in user_input_to_array
-            if aplhabet.include?(each_value) || aplhabet2.include?(each_value)
+            if ALPHABET.include?(each_value) 
             number_of_rotation.times do
                 each_value=each_value.next
                 end
