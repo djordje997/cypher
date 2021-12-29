865a92505c953f9abd6a6859dd63134e77a71e76
djordje997 <86196338+djordje997@users.noreply.github.com>
2021-12-29 16:41:04 +0100

    method in class are separathed now we can can do as shown in comment. Now we can call 'bin/rot number message' from terminal

--- bin/main.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require_relative '../lib/rot.rb'
-puts 'Please enter your text'
-user_input = gets.chomp
-puts 'Please enter number'
-input_number_of_rot=gets.chomp.to_i
-
-var=Rot.new.rot(input_number_of_rot,user_input)
--- lib/rot.rb
+++ lib/rot.rb
@@ -1,20 +1,20 @@
-ALPHABET= ("a".."z").to_a + ("A".."Z").to_a
+
 class Rot
-    
-    def rot(number_of_rotation, user_input)
-        if number_of_rotation > 26
-            p 'please enter number between 0-26'
-        end
-        user_input_to_array= user_input.split("")
+    ALPHABET= ("a".."z").to_a + ("A".."Z").to_a
+    def initialize(rot_key)
+        @rot_key=rot_key
+    end
+    def encrypt(message)
+        @message=message.split("")
         result=""
-        for each_value in user_input_to_array
-            if ALPHABET.include?(each_value) 
-            number_of_rotation.times do
-                each_value=each_value.next
+        for letter in @message
+            if ALPHABET.include?(letter)
+                @rot_key.times do 
+                    letter=letter.next
                 end
             end
-                result << each_value[-1] 
-            end
-    p result
+                result << letter[-1]
+        end
+        p result
     end
 end
