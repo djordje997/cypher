cbccf9de375681bfe65d91d16e562c65490fed72
djordje997 <djordjesarvan97@gmail.com>
2022-01-10 17:27:05 +0100

    identation fixed, method moved

--- lib/rot.rb
+++ lib/rot.rb
@@ -1,34 +1,33 @@
 class Rot
-    ALPHABET = (("a".."z").to_a + ("A".."Z").to_a).freeze
-    MAX_ROT_KEY = 26
+  ALPHABET = (("a".."z").to_a + ("A".."Z").to_a).freeze
+  MAX_ROT_KEY = 26
 
-    def initialize(rot_key)
-        @rot_key = rot_key
-        if @rot_key > MAX_ROT_KEY
-            raise ArgumentError, "Given rot key must be less than 26."
-        end
+  def initialize(rot_key)
+    @rot_key = rot_key
+    if @rot_key > MAX_ROT_KEY
+      raise ArgumentError, "Given rot key must be less than 26."
     end
+  end
 
-    def encrypt(message)
-        @message = message.split("")
-        @result = ""
-        for @letter in @message
+  def encryptable_letter?
+    ALPHABET.include?(@letter)
+  end
 
-            def encryptable_letter?
-                ALPHABET.include?(@letter)
-            end
+  def encrypt_letter
+    if encryptable_letter? 
+      @rot_key.times do 
+      @letter = @letter.next
+      end
+    end
+  end
 
-            def encrypt_letter
-                if encryptable_letter? 
-                    @rot_key.times do 
-                    @letter = @letter.next
-                    end
-                end
-            end
-
-            encrypt_letter
-            @result << @letter[-1]
-        end
-        @result     
-    end   
+  def encrypt(message)
+    @message = message.split("")
+    @result = ""
+    for @letter in @message
+      encrypt_letter
+      @result << @letter[-1]
+    end
+    @result  
+  end
 end
